3f929625e03df0489283c771d08beb57fa487e8a
goal651 <bugiriwilson651@gmail.com>
2025-05-10 15:08:44 +0200

    feature: api error handler

--- client/src/app/Home.tsx
+++ client/src/app/Home.tsx
@@ -1,18 +1,8 @@
-import { useEffect } from "react";
 import ChatScreen from "./sections/Chat";
 import UserGroup from "./sections/UserGroup";
-import { useNavigate } from "react-router-dom";
 
 export default function Home() {
-    const router = useNavigate()
 
-    // useEffect(() => {
-    //     const checkUser = () => {
-    //         const token = localStorage.getItem('token')
-    //         if (!token) router('/login')
-    //     }
-    //     checkUser()
-    // }, [router])
 
     return (
         <div className="flex w-full">
