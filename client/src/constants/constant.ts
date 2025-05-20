a54f3ca94afd2812fb015f7228ea3091f885670c
Wilson Goal BUGIRI <bugiriwilson651@gmail.com>
2025-05-20 21:05:51 +0200

    Update constant.ts

--- client/src/constants/constant.ts
+++ client/src/constants/constant.ts
@@ -1,6 +1,6 @@
 
 //const productionServer='https://chatterbox-production-9404.up.railway.app'
 
-export const webSocketUrl = 'http://localhost:3001'
+export const webSocketUrl = 'https://chatterbox-production-9404.up.railway.app'
 export const serverUrl = webSocketUrl + '/api'
 export const vapidKey = 'BEFnyePPNoUMVOxzMQDy2J8OBmq7AHnks8vNchYeVcMrNOC21ZSuXqfl04fs6DPmUIBTgP9olyKehQgpfZcTqJ0'
