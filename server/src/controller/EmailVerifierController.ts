98dbd47b25b43fb03e4fa813fd5e5a3ff6e926cd
Wilson Goal BUGIRI <bugiriwilson651@gmail.com>
2025-05-22 22:04:35 +0200

    Update EmailVerifierController.ts

--- server/src/controller/EmailVerifierController.ts
+++ server/src/controller/EmailVerifierController.ts
@@ -14,33 +14,33 @@ export default async function EmailVerifierController(req: Request, res: Respons
         const oneDay = 24 * 60 * 60 * 1000;
         const token = req.params.token
         if (!token) {
-            res.redirect('http://localhost:5173/login/error');
+            res.redirect('https://chatter-box-three.vercel.app/login/error');
             return
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as DecodedToken;
         if (!decoded) {
-            res.redirect('http://localhost:5173/login/error');
+            res.redirect('https://chatter-box-three.vercel.app/login/error');
             return
         }
         // Check if the token is expired
         const diff = decoded.exp - decoded.iat
         if (diff >= oneDay) {
-            res.redirect('http://localhost:5173/login/expired');
+            res.redirect('https://chatter-box-three.vercel.app/login/expired');
         }
         // Check if the user is already verified
         const user = await model.User.findById(decoded.userId);
         if (!user) {
-            res.redirect('http://localhost:5173/login/notfound');
+            res.redirect('https://chatter-box-three.vercel.app/login/notfound');
             return    
         }
 
         if (user.isVerified) {
-            res.redirect('http://localhost:5173/login/already');
+            res.redirect('https://chatter-box-three.vercel.app/login/already');
             return
         }
 
         await model.User.findByIdAndUpdate(user._id, { isVerified: true });
-        res.redirect('http://localhost:5173/login/success');
+        res.redirect('https://chatter-box-three.vercel.app/login/success');
     } catch (error) {
         console.error(error)
     }
